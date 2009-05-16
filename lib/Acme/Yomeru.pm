c31035931bddb1457cdcb3b7789dc6c43d8cf6e1
bayashi <bayashi@cpan.org>
2009-05-16 22:16:36 +0900

    first commit

---
+++ lib/Acme/Yomeru.pm
@@ -0,0 +1,60 @@
+package Acme::Yomeru;
+
+use warnings;
+use strict;
+use Carp;
+
+use version; our $VERSION = qv('0.0.1');
+
+# Other recommended modules (uncomment to use):
+#  use IO::Prompt;
+#  use Perl6::Export;
+#  use Perl6::Slurp;
+#  use Perl6::Say;
+
+
+# Module implementation here
+
+
+1;
+
+__END__
+
+
+=head1 NAME
+
+Acme::Yomeru - [One line description]
+
+
+=head1 SYNOPSIS
+
+    use Acme::Yomeru;
+
+
+=head1 METHOD
+
+=over
+
+=item new(I<$arg>)
+
+constructor
+
+=item method_name
+
+description of method
+
+=back
+
+
+=head1 AUTHOR
+
+Copyright (c) 2009, Dai Okabayashi C<< <bayashi@cpan.org> >>
+
+
+=head1 LICENCE
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
+
+=cut
+
---
+++ t/00.load.t
@@ -0,0 +1,7 @@
+use Test::More tests => 1;
+
+BEGIN {
+use_ok( 'Acme::Yomeru' );
+}
+
+diag( "Testing Acme::Yomeru $Acme::Yomeru::VERSION" );
